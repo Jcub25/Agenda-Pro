a9e548ea4fdd509133e05a4c5f89f28909c872a9
Jeanca <jcubillo701@gmail.com>
2025-10-18 15:35:33 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AgendaPro
+{
+    // Creacion de la clase persona con constructor
+    class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public Persona(int id, string nombre, string telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Telefono = telefono;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} | {Nombre} | {Telefono}";
+        }
+    }
+
+    // Creacion de clase Cita con atributos
+    class Cita
+    {
+        public int PersonaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+
+        public Cita(int personaId, DateTime fecha, string descripcion)
+        {
+            PersonaId = personaId;
+            Fecha = fecha;
+            Descripcion = descripcion;
+        }
+
+        public override string ToString()
+        {
+            return $"{PersonaId} | {Fecha.ToShortDateString()} | {Descripcion}";
+        }
+    }
