ca7fbfd129d986ea104ab84c3de820ab1fbf0466
Jeanca <jcubillo701@gmail.com>
2025-10-18 15:46:48 -0600

    Creacion de menu principal usando while y el switch case

--- Program.cs
+++ Program.cs
@@ -42,3 +42,57 @@ namespace AgendaPro
             return $"{PersonaId} | {Fecha.ToShortDateString()} | {Descripcion}";
         }
     }
+    // Programa principal Menu
+    class Program
+    {
+        static void Main()
+        {
+            List<Persona> personas = new List<Persona>();
+            List<Cita> citas = new List<Cita>();
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.WriteLine("\nMENU PRINCIPAL - AgendaPro");
+                Console.WriteLine("1. Registrar persona");
+                Console.WriteLine("2. Listar personas");
+                Console.WriteLine("3. Crear cita");
+                Console.WriteLine("4. Listar citas por PersonaId");
+                Console.WriteLine("5. Mostrar todas las citas");
+                Console.WriteLine("6. Salir");
+                Console.Write("Seleccione una opcion: ");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        RegistrarPersona(personas);
+                        break;
+
+                    case "2":
+                        ListarPersonas(personas);
+                        break;
+
+                    case "3":
+                        CrearCita(personas, citas);
+                        break;
+
+                    case "4":
+                        ListarCitasPorPersona(citas);
+                        break;
+
+                    case "5":
+                        MostrarTodasLasCitas(citas);
+                        break;
+
+                    case "6":
+                        salir = true;
+                        Console.WriteLine("Saliendo del sistema...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción invalida. Intente de nuevo.");
+                        break;
+                }
+            }
+        }
